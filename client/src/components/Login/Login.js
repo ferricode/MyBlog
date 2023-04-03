012b9fe88440fa8e8f42ce2185c8c1058bf7a694
ferricode <ferided88@gmail.com>
2023-04-04 01:13:22 +0300

    Login and Register functionalities added.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,13 +1,36 @@
+import { useContext } from 'react';
+import { useForm } from '../../hooks/useForm';
+
+import { AuthContext } from '../../contexts/AuthContext';
 
 export const Login = () => {
+    const { onLoginSubmit } = useContext(AuthContext);
+    const { values, changeHandler, onSubmit } = useForm({
+        email: '',
+        password: ''
+    }, onLoginSubmit);
     return (
         <div className="col-md-10 col-lg-4 m-auto">
-            <form className="form-group">
+            <form method="POST" className="form-group" onSubmit={onSubmit}>
                 <h2 className="title mb-2">Вписване</h2>
-                <input type="text" size="50" className="form-control" placeholder="Email адрес" />
-                <input type="password" name="password" className="form-control" placeholder="Парола" />
-                <button type="submit" className="form-control">Вписване</button>
+                <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="Email адрес"
+                    value={values.email}
+                    onChange={changeHandler}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    placeholder="Парола"
+                    value={values.password}
+                    onChange={changeHandler}
+                />
+                <input type="submit" className="form-control" value="Вписване" />
             </form>
-        </div>
+        </div >
     );
 };
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,12 +1,48 @@
+import { useContext } from 'react';
+import { useForm } from '../../hooks/useForm';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export const Register = () => {
+    const { onRegisterSubmit } = useContext(AuthContext);
+    const { values, changeHandler, onSubmit } = useForm({
+        userName: '',
+        email: '',
+        password: '',
+        repeatPassword: ''
+    }, onRegisterSubmit);
+
     return (
         <div className="col-md-10 col-lg-4 m-auto">
-            <form className="form-group">
+            <form method="POST" className="form-group" onSubmit={onSubmit}>
                 <h2 className="title mb-2">Регистрация</h2>
-                <input type="text" size="50" className="form-control" placeholder="Email адрес..." />
-                <input type="password" name="password" className="form-control" placeholder="Парола..." />
-                <input type="password" name="repeatPassword" className="form-control" placeholder="Повтори паролата..." />
+                <input
+                    type="text"
+                    name="userName"
+                    className="form-control"
+                    placeholder="Потребителско име"
+                    value={values.userName}
+                    onChange={changeHandler} />
+                <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="Email адрес"
+                    value={values.email}
+                    onChange={changeHandler} />
+                <input
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    placeholder="Парола"
+                    value={values.password}
+                    onChange={changeHandler} />
+                <input
+                    type="password"
+                    name="repeatPassword"
+                    className="form-control"
+                    placeholder="Повтори паролата"
+                    value={values.repeatPassword}
+                    onChange={changeHandler} />
                 <input type="submit" className="form-control" value="Регистрирай се" />
             </form>
         </div>

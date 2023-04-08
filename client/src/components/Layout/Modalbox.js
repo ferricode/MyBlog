4620d78effb0cf1061515f1b64117c5b09628a5d
ferricode <ferided88@gmail.com>
2023-04-08 22:25:47 +0300

    Modalbox edited, video added.

--- client/src/components/Layout/Modalbox.js
+++ client/src/components/Layout/Modalbox.js
@@ -2,9 +2,7 @@ export const Modalbox = () => {
     return (
         <div className="modalBox">
             <div className="modalBox-body">
-                <iframe title="video" width="100%" height="450px" className="border-0"
-                    src="https://www.youtube.com/watch?v=2VB6YYHIECQ">
-                </iframe>
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/2VB6YYHIECQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
         </div>
     );
--- client/src/components/Layout/ThemeSelector.js
+++ client/src/components/Layout/ThemeSelector.js
@@ -1,12 +1,12 @@
 export const ThemeSelector = () => {
     return (
         <div className="theme-selector">
-            <a href="#" className="spinner">
+            <a href={void (0)} className="spinner">
                 <i className="ti-paint-bucket"></i>
             </a>
             <div className="body">
-                <a href="#" className="light"></a>
-                <a href="#" className="dark"></a>
+                <a href={void (0)} className="light"></a>
+                <a href={void (0)} className="dark"></a>
             </div>
         </div>
 

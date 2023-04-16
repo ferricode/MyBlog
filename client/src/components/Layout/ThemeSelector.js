d8db470bf1ab27459874bb84323ca60b0be2738a
ferricode <ferided88@gmail.com>
2023-04-16 14:22:54 +0300

    ThemeSelector keeps a state.

--- client/src/components/Layout/ThemeSelector.js
+++ client/src/components/Layout/ThemeSelector.js
@@ -1,16 +1,24 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/anchor-has-content */
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+
 export const ThemeSelector = () => {
+    const [theme, setTheme] = useState('light');
+
+    const handleThemeChange = (newTheme) => {
+        setTheme(newTheme);
+    };
+
     return (
         <div className="theme-selector">
             <a href={void (0)} className="spinner">
                 <i className="ti-paint-bucket"></i>
             </a>
             <div className="body">
-                <a href={void (0)} className="light"></a>
-                <a href={void (0)} className="dark"></a>
+                <Link to={void (0)} onClick={() => handleThemeChange('light')} className={`light ${theme === 'light' ? 'active' : ''}`}>
+                </Link>
+                <Link to={void (0)} onClick={() => handleThemeChange('dark')} className={`dark ${theme === 'dark' ? 'active' : ''}`}>
+                </Link>
             </div>
         </div>
-
     );
 };

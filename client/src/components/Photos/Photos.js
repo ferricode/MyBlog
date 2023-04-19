396d22be410b0268d5d0483a471996b63b7c5e09
ferricode <ferided88@gmail.com>
2023-04-19 10:34:12 +0300

    Authentication for create in posts and photos.

--- client/src/components/Photos/Photos.js
+++ client/src/components/Photos/Photos.js
@@ -1,16 +1,18 @@
 import { usePhotoContext } from "../../contexts/PhotoContext";
+import { useAuthContext } from "../../contexts/AuthContext";
 import { Link } from 'react-router-dom';
 import { PhotoItem } from "./PhotoItem/PhotoItem";
 
 export const Photos = () => {
     const { photos } = usePhotoContext();
-
+    const { isAuthenticated } = useAuthContext();
+    console.log(photos);
     return (
         <div className="page-container">
             <h1 className="title">Снимкария:</h1>
             <br />
             <div className="col-md-10 col-lg-4 m-auto">
-                <Link to="/create-photo" className="nav-link"><input type="submit" className="form-control" value="Качи снимка:" /></Link>
+                {isAuthenticated && <Link to="/create-photo" className="nav-link"><input type="submit" className="form-control" value="Качи снимка:" /></Link>}
             </div>
             <br />
             <div className="row mb-4">
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,16 +1,18 @@
 import { usePostContext } from "../../contexts/PostContext";
+import { useAuthContext } from "../../contexts/AuthContext";
 import { Link } from 'react-router-dom';
 import { PostItem } from "./PostItem/PostItem";
 
 export const Posts = () => {
     const { posts } = usePostContext();
+    const { isAuthenticated } = useAuthContext();
 
     return (
         <div className="page-container">
             <h1 className="title">Всички писания:</h1>
             <br />
             <div className="col-md-10 col-lg-4 m-auto">
-                <Link to="/create-post" className="nav-link"><input type="submit" className="form-control" value="Напиши ново..." /></Link>
+                {isAuthenticated && <Link to="/create-post" className="nav-link"><input type="submit" className="form-control" value="Напиши ново..." /></Link>}
             </div>
             <br />
             <div className="row mb-4">
